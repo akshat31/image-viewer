84bbbdaec7529c9425ce32267851c3ba4377f17b
asoni-ctr <asoni.ctr@tenable.com>
2020-11-30 22:44:59 +0530

    Intial commit, created folder structure for project and all intial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./screens/header/header";
+import { Home } from "./screens/home/home";
+import { Login } from "./screens/login/login";
+import { Profile } from "./screens/profile/profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className='container'>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/profile" component={Profile} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
